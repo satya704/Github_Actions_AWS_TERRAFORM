c74640ad8880cf8c5f1e1c12c2850bf3974da01b
satyagsn704 <satyagsn704@gmail.com>
2026-03-16 17:50:23 +0530

    Added Terraform GitHub Actions workflow

---
+++ main.tf
@@ -0,0 +1,9 @@
+# Creating VPC
+resource "aws_vpc" "alight-vpc" {
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  tags {
+    vpc_name = "Alight-vpc"
+  }
+}
